873170cf28bc6d0adea144ea4ffc3a1de4f8653b
Alexandros Tsichouridis <alexchih02@gmail.com>
2019-11-19 13:46:12 +0200

    Added jpa repositories

--- src/main/java/io/alexc/classroomdemo/repository/ClassroomRepository.java
+++ src/main/java/io/alexc/classroomdemo/repository/ClassroomRepository.java
@@ -2,9 +2,11 @@ package io.alexc.classroomdemo.repository;
 
 import io.alexc.classroomdemo.entity.Classroom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ClassroomRepository extends JpaRepository<Classroom, Integer> {
 
     @Override
--- src/main/java/io/alexc/classroomdemo/repository/StudentRepository.java
+++ src/main/java/io/alexc/classroomdemo/repository/StudentRepository.java
@@ -2,9 +2,11 @@ package io.alexc.classroomdemo.repository;
 
 import io.alexc.classroomdemo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
     @Override
